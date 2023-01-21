8a1fb578a7777cd4de246728422c6ff3a92cf4cb
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-21 16:52:08 +0900

    feat: board, reply relation 되는 부분 테스트 코드 작성

--- src/test/java/org/zerock/springboot_board/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/springboot_board/repository/BoardRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.springboot_board.enitty.Board;
 import org.zerock.springboot_board.enitty.Member;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -29,5 +30,16 @@ public class BoardRepositoryTests {
 
             boardRepository.save(board);
         });
+    }
+
+    @Test
+    public void testRead1(){
+
+        Optional<Board> result = boardRepository.findById(100L);
+
+        Board board = result.get();
+
+        System.out.println(board);
+        System.out.println(board.getWriter());
     }
 }
--- src/test/java/org/zerock/springboot_board/repository/ReplyRepositoryTests.java
+++ src/test/java/org/zerock/springboot_board/repository/ReplyRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.springboot_board.enitty.Board;
 import org.zerock.springboot_board.enitty.Reply;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -32,5 +33,15 @@ public class ReplyRepositoryTests {
 
             replyRepository.save(reply);
         });
+    }
+
+    @Test
+    public void readReply1(){
+        Optional<Reply> result = replyRepository.findById(1L);
+
+        Reply reply = result.get();
+
+        System.out.println(reply);
+        System.out.println(reply.getBoard());
     }
 }

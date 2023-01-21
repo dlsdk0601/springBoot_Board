484cb9839233ab048f9a272bf5aba0b4b154d3b6
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-21 17:23:20 +0900

    refactor: board repository 에 join 쿼리 추가하고, 테스트 코드 작성

--- src/main/java/org/zerock/springboot_board/repository/BoardRepository.java
+++ src/main/java/org/zerock/springboot_board/repository/BoardRepository.java
@@ -1,7 +1,12 @@
 package org.zerock.springboot_board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.springboot_board.enitty.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+//    한개의 low 내에 list 로 나옴
+    @Query("select b, w from Board b left join b.writer w where b.bno =:bno")
+    Object getBoardWithWriter(@Param("bno") Long bno);
 }

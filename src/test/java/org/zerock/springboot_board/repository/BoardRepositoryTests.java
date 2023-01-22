5d3482f67bc82780da87d78857109e8fa0f8ff91
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-22 23:28:52 +0900

    feat: 게시글과 댓글 join test code 작성

--- src/test/java/org/zerock/springboot_board/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/springboot_board/repository/BoardRepositoryTests.java
@@ -3,11 +3,16 @@ package org.zerock.springboot_board.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.springboot_board.enitty.Board;
 import org.zerock.springboot_board.enitty.Member;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -47,12 +52,34 @@ public class BoardRepositoryTests {
     }
 
     @Test
-    public void testReadWuthWriter(){
+    public void testReadWithWriter(){
         Object result = boardRepository.getBoardWithWriter(100L);
         Object[] arr = (Object[])result;
 
         System.out.println("::::::::::::::::::::::::::::::::::;");
         System.out.println(Arrays.toString(arr));
 
+    }
+
+    @Test
+    public void testGetBoardWithReply(){
+        List<Object[]> result = boardRepository.getBoardWithReply(100L);
+
+        for (Object[] arr : result){
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    @Test
+    public void testWithReplyCount(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row -> {
+            Object[] arr = (Object[])row;
+
+            System.out.println(Arrays.toString(arr));
+        });
     }
 }

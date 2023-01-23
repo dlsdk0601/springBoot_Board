d5ef80f70f75a1278e1a490039ee04a476a3e271
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-23 14:38:38 +0900

    feat: board service 테스트 코드 생성

---
+++ src/test/java/org/zerock/springboot_board/service/BoardServiceTests.java
@@ -0,0 +1,25 @@
+package org.zerock.springboot_board.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.springboot_board.dto.BoardDTO;
+
+@SpringBootTest
+public class BoardServiceTests {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Test
+    public void testRegister(){
+
+        BoardDTO dto = BoardDTO.builder()
+                .title("Test..")
+                .content("test.........")
+                .writerEmail("test@sest.com")
+                .build();
+
+        Long bno = boardService.register(dto);
+    }
+}

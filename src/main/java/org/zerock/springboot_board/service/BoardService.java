9822ec51220cafb4da9d315ddbe9d7260da0901e
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-24 23:36:40 +0900

    feat: board 목록 조회 화면단에서 필요한 service, serviceImpl 클래스 작성

--- src/main/java/org/zerock/springboot_board/service/BoardService.java
+++ src/main/java/org/zerock/springboot_board/service/BoardService.java
@@ -1,6 +1,8 @@
 package org.zerock.springboot_board.service;
 
 import org.zerock.springboot_board.dto.BoardDTO;
+import org.zerock.springboot_board.dto.PageRequestDTO;
+import org.zerock.springboot_board.dto.PageResultDTO;
 import org.zerock.springboot_board.enitty.Board;
 import org.zerock.springboot_board.enitty.Member;
 
@@ -8,16 +10,37 @@ public interface BoardService {
 
     Long register(BoardDTO dto);
 
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
+
     default Board dtoToEntity(BoardDTO dto){
 
-        Member member = Member.builder().email(dto.getWriterEmail()).build();
+        Member member = Member
+                .builder()
+                .email(dto.getWriterEmail())
+                .build();
 
-        Board board = Board.builder().bno(dto.getBno())
+        return Board.builder()
+                .bno(dto.getBno())
                 .title(dto.getTitle())
                 .content(dto.getContent())
                 .writer(member)
                 .build();
+    }
 
-        return board;
+    // BoardService 인터페이스에 추가하는 entityToDTO()
+    default BoardDTO entityToDTO(Board board, Member member, Long replyCount){
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .writerEmail(member.getEmail())
+                .writerName(member.getName())
+                .replyCount(replyCount.intValue())
+                .build();
+
+        return boardDTO;
     }
 }
--- src/main/java/org/zerock/springboot_board/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/springboot_board/service/BoardServiceImpl.java
@@ -2,10 +2,17 @@ package org.zerock.springboot_board.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.zerock.springboot_board.dto.BoardDTO;
+import org.zerock.springboot_board.dto.PageRequestDTO;
+import org.zerock.springboot_board.dto.PageResultDTO;
 import org.zerock.springboot_board.enitty.Board;
+import org.zerock.springboot_board.enitty.Member;
 import org.zerock.springboot_board.repository.BoardRepository;
+
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +31,18 @@ public class BoardServiceImpl implements BoardService{
         repository.save(board);
 
         return board.getBno();
+    }
+
+    @Override
+    public PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO){
+        log.info(pageRequestDTO);
+
+        Function<Object[], BoardDTO> fn = (en -> entityToDTO((Board)en[0], (Member)en[1], (Long)en[2]));
+
+        Page<Object[]> result = repository.getBoardWithReplyCount(
+                pageRequestDTO.getPageable(Sort.by("bno").descending()));
+        )
+
+        return new PageResultDTO<>(result, fn);
     }
 }

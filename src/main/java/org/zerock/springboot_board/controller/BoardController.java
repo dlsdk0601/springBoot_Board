abe8ae7b1aa0bde21ae946c73b7460a2b9b0405c
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-27 00:20:04 +0900

    feat: register, read 컨트롤러 생성

--- src/main/java/org/zerock/springboot_board/controller/BoardController.java
+++ src/main/java/org/zerock/springboot_board/controller/BoardController.java
@@ -5,7 +5,11 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.springboot_board.dto.BoardDTO;
 import org.zerock.springboot_board.dto.PageRequestDTO;
 import org.zerock.springboot_board.service.BoardService;
 
@@ -22,5 +26,36 @@ public class BoardController {
         log.info("list ..." + pageRequestDTO);
 
         model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    private void register(){
+        log.info("register.....");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
+        // Member 테이블에 존재하는 이메일로 작성해야한다.
+        log.info("dto...." + dto);
+
+        Long bno = boardService.register(dto);
+
+        log.info("BNO : " + bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/read")
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model){
+
+        log.info("bno : " + bno);
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
     }
 }

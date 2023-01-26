0dcc283da5596a19a04a01f11b00c084d2b2345c
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-26 21:54:35 +0900

    feat: 제목과 내용에 한해서 수정이 이뤄지게 하기 위해 메서드를 추가한다.

--- src/main/java/org/zerock/springboot_board/enitty/Board.java
+++ src/main/java/org/zerock/springboot_board/enitty/Board.java
@@ -22,4 +22,14 @@ public class Board extends BaseEntity{
 
     @ManyToOne(fetch = FetchType.LAZY) // 명시적으로 lazy 로딩 처리
     private Member writer;
+
+    // 제목과 내용에 한해서 수정이 이뤄지게 하기 위해
+    // 메서드를 추가한다.
+    public void changeTitle(String title){
+        this.title = title;
+    }
+
+    public void changeContent(String content){
+        this.content = content;
+    }
 }

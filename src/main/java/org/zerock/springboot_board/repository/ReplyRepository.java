ddf3bcb629abfb472e37b91c02fa1cd0c25896fc
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-26 21:42:50 +0900

    feat: bno pk를 받아서 상세 조회하는 로직 생성

--- src/main/java/org/zerock/springboot_board/repository/ReplyRepository.java
+++ src/main/java/org/zerock/springboot_board/repository/ReplyRepository.java
@@ -1,8 +1,13 @@
 package org.zerock.springboot_board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.springboot_board.enitty.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    @Modifying // update, delete 를 실행하기 위해 필요한 어노테이션이다.
+    @Query("delete from Reply r where r.board.bno = :bno")
+    void deleteByBno(Long bno);
 
 }

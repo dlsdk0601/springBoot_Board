86ea8efd08f67b17571edbdf3d11ea7299916818
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-27 23:25:42 +0900

    feat: / 라우트에서 board/list로 리다이렉트 시키는 controller 생성

---
+++ src/main/java/org/zerock/springboot_board/controller/BaseController.java
@@ -0,0 +1,17 @@
+package org.zerock.springboot_board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class BaseController {
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/board/list";
+    }
+}

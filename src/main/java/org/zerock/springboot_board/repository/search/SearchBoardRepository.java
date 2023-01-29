5f94bf4e5cbe746a60a6a2427661933a601c3f46
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-29 21:51:14 +0900

    feat: searchPage 메서드 생성

--- src/main/java/org/zerock/springboot_board/repository/search/SearchBoardRepository.java
+++ src/main/java/org/zerock/springboot_board/repository/search/SearchBoardRepository.java
@@ -1,8 +1,12 @@
 package org.zerock.springboot_board.repository.search;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.zerock.springboot_board.enitty.Board;
 
 public interface SearchBoardRepository {
 
     Board search1();
+
+    Page<Object[]> searchPage(String type, String keyword, Pageable pageable);
 }
